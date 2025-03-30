31f40feb4a29c7cae766021ed2fed79b3c71e746
Howard-18844 <hkh1216@connect.hku.hk>
2025-03-30 17:17:39 +0800

    delete testing thing2

--- manage.py
+++ manage.py
@@ -20,48 +20,7 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-def get_address_info(query):
-    url = f"https://www.als.gov.hk/lookup?q={query}"
-    
-    # Add custom headers
-    headers = {
-        'Accept': 'application/json',  # specify accepted response format
-    }
 
-    response = requests.get(url, headers=headers)
-
-    #print("Status Code:", response.status_code)
-    #print("Response Content:", response.text)  
-
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            return extract_address_info(data)
-        except json.JSONDecodeError:
-            print("Error: Response is not in JSON format")
-            return None
-    else:
-        print("Error fetching data from API:", response.status_code)
-        return None
-
-def extract_address_info(json_data):
-    try:
-        first_suggestion = json_data.get("SuggestedAddress", [])[0].get("Address", {}).get("PremisesAddress", {})
-        chi_premises = first_suggestion.get("ChiPremisesAddress", {})
-        geospatial = first_suggestion.get("GeospatialInformation", {})
-        
-        estate_name = chi_premises.get("ChiEstate", {}).get("EstateName", "N/A")
-        latitude = geospatial.get("Latitude", "N/A")
-        longitude = geospatial.get("Longitude", "N/A")
-        
-        return {
-            "EstateName": estate_name,
-            "Latitude": latitude,
-            "Longitude": longitude
-        }
-    except IndexError:
-        print("Error: No valid address found.")
-        return None
 
 
 
